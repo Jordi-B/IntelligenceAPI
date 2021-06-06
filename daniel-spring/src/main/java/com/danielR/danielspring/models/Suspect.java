cfc591a5fcb28d7f69ab5afa42e0bbb422f6dc00
or <orerez09@gmail.com>
2021-06-06 17:14:06 +0300

    feat(models): making the models

--- daniel-spring/src/main/java/com/danielR/danielspring/models/Suspect.java
+++ daniel-spring/src/main/java/com/danielR/danielspring/models/Suspect.java
@@ -1,4 +1,28 @@
 package com.danielR.danielspring.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Suspect {
+    @Id
+    @OneToOne(mappedBy = "id")
+    @JoinColumn(name = "person_id")
+    private String personId;
+
+    @Column(name = "is_wanted")
+    private boolean isWanted;
+
+    @Column(name = "started")
+    private Date started;
 }
