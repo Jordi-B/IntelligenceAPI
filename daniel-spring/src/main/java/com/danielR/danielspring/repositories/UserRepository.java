d2f79e66db9947a72f03d066d5264d680eedca89
hackathon IDF <hack@cs.colman.ac.il>
2021-06-06 15:59:35 +0300

    feat: adds repository files.

---
+++ daniel-spring/src/main/java/com/danielR/danielspring/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.danielR.danielspring.repositories;
+
+import com.danielR.danielspring.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
