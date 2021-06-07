48a9eeaaf54c4f5fc69d64c947413e4e3200ea5b
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 11:47:29 +0300

    feat(wordService): add contain function

--- daniel-spring/src/main/java/com/danielR/danielspring/services/PostService.java
+++ daniel-spring/src/main/java/com/danielR/danielspring/services/PostService.java
@@ -1,7 +1,20 @@
 package com.danielR.danielspring.services;
 
+import com.danielR.danielspring.models.Post;
+import com.danielR.danielspring.models.Word;
+import com.danielR.danielspring.repositories.PostRepository;
+import com.danielR.danielspring.repositories.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public List<Post> getPostsContainWord(String word) {
+        return this.repository.findByTextContaining(word);
+    }
 }
--- daniel-spring/src/main/java/com/danielR/danielspring/services/WordService.java
+++ daniel-spring/src/main/java/com/danielR/danielspring/services/WordService.java
@@ -1,7 +1,38 @@
 package com.danielR.danielspring.services;
 
+import com.danielR.danielspring.models.Post;
+import com.danielR.danielspring.models.Suspect;
+import com.danielR.danielspring.models.Word;
+import com.danielR.danielspring.repositories.SuspectRepository;
+import com.danielR.danielspring.repositories.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WordService {
+    @Autowired
+    private WordRepository repository;
+
+    private PostService postService;
+
+    public void addWord(String newWord) {
+        Word word = new Word();
+        word.setWord(newWord);
+
+
+        this.repository.save(word);
+    }
+
+    private int getWordCounter(String word){
+        List<Post> posts = postService.getPostsContainWord(word);
+        int counter = 0;
+
+        for(Post post : posts){
+            counter += post.getText().split(word).length - 1;
+        }
+
+        return counter;
+    }
 }
